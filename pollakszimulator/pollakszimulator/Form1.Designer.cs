3783baa06548f8387ff205e2e754a921f6eafa66
Schmiz Otto <52161487+zolotvok@users.noreply.github.com>
2022-04-27 09:17:29 +0200

    Add files via upload

---
+++ pollakszimulator/pollakszimulator/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace pollakszimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studybtn = new System.Windows.Forms.Button();
+            this.studytb = new System.Windows.Forms.TextBox();
+            this.playetb = new System.Windows.Forms.TextBox();
+            this.playbtn = new System.Windows.Forms.Button();
+            this.partytb = new System.Windows.Forms.TextBox();
+            this.partybtn = new System.Windows.Forms.Button();
+            this.eattb = new System.Windows.Forms.TextBox();
+            this.eatbtn = new System.Windows.Forms.Button();
+            this.showertb = new System.Windows.Forms.TextBox();
+            this.showerbtn = new System.Windows.Forms.Button();
+            this.sleeptb = new System.Windows.Forms.TextBox();
+            this.sleepbtn = new System.Windows.Forms.Button();
+            this.daycount = new System.Windows.Forms.Label();
+            this.storylbl = new System.Windows.Forms.Label();
+            this.hoursleftlbl = new System.Windows.Forms.Label();
+            this.endDaybtn = new System.Windows.Forms.Button();
+            this.intelligencelbl = new System.Windows.Forms.Label();
+            this.happinesslbl = new System.Windows.Forms.Label();
+            this.hungerlbl = new System.Windows.Forms.Label();
+            this.showerlbl = new System.Windows.Forms.Label();
+            this.sleeplbl = new System.Windows.Forms.Label();
+            this.outcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // studybtn
+            // 
+            this.studybtn.Location = new System.Drawing.Point(72, 394);
+            this.studybtn.Name = "studybtn";
+            this.studybtn.Size = new System.Drawing.Size(75, 30);
+            this.studybtn.TabIndex = 0;
+            this.studybtn.Text = "Tanulás";
+            this.studybtn.UseVisualStyleBackColor = true;
+            this.studybtn.Click += new System.EventHandler(this.studybtn_Click);
+            // 
+            // studytb
+            // 
+            this.studytb.Location = new System.Drawing.Point(72, 368);
+            this.studytb.Name = "studytb";
+            this.studytb.Size = new System.Drawing.Size(75, 20);
+            this.studytb.TabIndex = 1;
+            // 
+            // playetb
+            // 
+            this.playetb.Location = new System.Drawing.Point(178, 368);
+            this.playetb.Name = "playetb";
+            this.playetb.Size = new System.Drawing.Size(75, 20);
+            this.playetb.TabIndex = 3;
+            // 
+            // playbtn
+            // 
+            this.playbtn.Location = new System.Drawing.Point(178, 394);
+            this.playbtn.Name = "playbtn";
+            this.playbtn.Size = new System.Drawing.Size(75, 30);
+            this.playbtn.TabIndex = 2;
+            this.playbtn.Text = "Játék";
+            this.playbtn.UseVisualStyleBackColor = true;
+            this.playbtn.Click += new System.EventHandler(this.playbtn_Click);
+            // 
+            // partytb
+            // 
+            this.partytb.Location = new System.Drawing.Point(292, 368);
+            this.partytb.Name = "partytb";
+            this.partytb.Size = new System.Drawing.Size(75, 20);
+            this.partytb.TabIndex = 5;
+            // 
+            // partybtn
+            // 
+            this.partybtn.Location = new System.Drawing.Point(292, 394);
+            this.partybtn.Name = "partybtn";
+            this.partybtn.Size = new System.Drawing.Size(75, 30);
+            this.partybtn.TabIndex = 4;
+            this.partybtn.Text = "Buli";
+            this.partybtn.UseVisualStyleBackColor = true;
+            this.partybtn.Click += new System.EventHandler(this.partybtn_Click);
+            // 
+            // eattb
+            // 
+            this.eattb.Location = new System.Drawing.Point(397, 368);
+            this.eattb.Name = "eattb";
+            this.eattb.Size = new System.Drawing.Size(75, 20);
+            this.eattb.TabIndex = 7;
+            // 
+            // eatbtn
+            // 
+            this.eatbtn.Location = new System.Drawing.Point(397, 394);
+            this.eatbtn.Name = "eatbtn";
+            this.eatbtn.Size = new System.Drawing.Size(75, 30);
+            this.eatbtn.TabIndex = 6;
+            this.eatbtn.Text = "Evés";
+            this.eatbtn.UseVisualStyleBackColor = true;
+            this.eatbtn.Click += new System.EventHandler(this.eatbtn_Click);
+            // 
+            // showertb
+            // 
+            this.showertb.Location = new System.Drawing.Point(513, 368);
+            this.showertb.Name = "showertb";
+            this.showertb.Size = new System.Drawing.Size(75, 20);
+            this.showertb.TabIndex = 9;
+            // 
+            // showerbtn
+            // 
+            this.showerbtn.Location = new System.Drawing.Point(513, 394);
+            this.showerbtn.Name = "showerbtn";
+            this.showerbtn.Size = new System.Drawing.Size(75, 30);
+            this.showerbtn.TabIndex = 8;
+            this.showerbtn.Text = "Fürdés";
+            this.showerbtn.UseVisualStyleBackColor = true;
+            this.showerbtn.Click += new System.EventHandler(this.showerbtn_Click);
+            // 
+            // sleeptb
+            // 
+            this.sleeptb.Location = new System.Drawing.Point(634, 368);
+            this.sleeptb.Name = "sleeptb";
+            this.sleeptb.Size = new System.Drawing.Size(75, 20);
+            this.sleeptb.TabIndex = 11;
+            // 
+            // sleepbtn
+            // 
+            this.sleepbtn.Location = new System.Drawing.Point(634, 394);
+            this.sleepbtn.Name = "sleepbtn";
+            this.sleepbtn.Size = new System.Drawing.Size(75, 30);
+            this.sleepbtn.TabIndex = 10;
+            this.sleepbtn.Text = "Alvás";
+            this.sleepbtn.UseVisualStyleBackColor = true;
+            this.sleepbtn.Click += new System.EventHandler(this.sleepbtn_Click);
+            // 
+            // daycount
+            // 
+            this.daycount.AutoSize = true;
+            this.daycount.Location = new System.Drawing.Point(36, 20);
+            this.daycount.Name = "daycount";
+            this.daycount.Size = new System.Drawing.Size(35, 13);
+            this.daycount.TabIndex = 12;
+            this.daycount.Text = "label1";
+            // 
+            // storylbl
+            // 
+            this.storylbl.AutoSize = true;
+            this.storylbl.Location = new System.Drawing.Point(359, 196);
+            this.storylbl.Name = "storylbl";
+            this.storylbl.Size = new System.Drawing.Size(92, 13);
+            this.storylbl.TabIndex = 14;
+            this.storylbl.Text = "Egy átlagos nap:(:";
+            this.storylbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hoursleftlbl
+            // 
+            this.hoursleftlbl.AutoSize = true;
+            this.hoursleftlbl.Location = new System.Drawing.Point(72, 333);
+            this.hoursleftlbl.Name = "hoursleftlbl";
+            this.hoursleftlbl.Size = new System.Drawing.Size(35, 13);
+            this.hoursleftlbl.TabIndex = 15;
+            this.hoursleftlbl.Text = "label3";
+            // 
+            // endDaybtn
+            // 
+            this.endDaybtn.Location = new System.Drawing.Point(727, 368);
+            this.endDaybtn.Name = "endDaybtn";
+            this.endDaybtn.Size = new System.Drawing.Size(61, 60);
+            this.endDaybtn.TabIndex = 16;
+            this.endDaybtn.Text = "A nap vége";
+            this.endDaybtn.UseVisualStyleBackColor = true;
+            this.endDaybtn.Click += new System.EventHandler(this.endDaybtn_Click);
+            // 
+            // intelligencelbl
+            // 
+            this.intelligencelbl.AutoSize = true;
+            this.intelligencelbl.Location = new System.Drawing.Point(674, 37);
+            this.intelligencelbl.Name = "intelligencelbl";
+            this.intelligencelbl.Size = new System.Drawing.Size(35, 13);
+            this.intelligencelbl.TabIndex = 17;
+            this.intelligencelbl.Text = "label1";
+            // 
+            // happinesslbl
+            // 
+            this.happinesslbl.AutoSize = true;
+            this.happinesslbl.Location = new System.Drawing.Point(674, 71);
+            this.happinesslbl.Name = "happinesslbl";
+            this.happinesslbl.Size = new System.Drawing.Size(35, 13);
+            this.happinesslbl.TabIndex = 18;
+            this.happinesslbl.Text = "label2";
+            // 
+            // hungerlbl
+            // 
+            this.hungerlbl.AutoSize = true;
+            this.hungerlbl.Location = new System.Drawing.Point(674, 105);
+            this.hungerlbl.Name = "hungerlbl";
+            this.hungerlbl.Size = new System.Drawing.Size(35, 13);
+            this.hungerlbl.TabIndex = 19;
+            this.hungerlbl.Text = "label3";
+            // 
+            // showerlbl
+            // 
+            this.showerlbl.AutoSize = true;
+            this.showerlbl.Location = new System.Drawing.Point(674, 146);
+            this.showerlbl.Name = "showerlbl";
+            this.showerlbl.Size = new System.Drawing.Size(35, 13);
+            this.showerlbl.TabIndex = 20;
+            this.showerlbl.Text = "label4";
+            // 
+            // sleeplbl
+            // 
+            this.sleeplbl.AutoSize = true;
+            this.sleeplbl.Location = new System.Drawing.Point(674, 185);
+            this.sleeplbl.Name = "sleeplbl";
+            this.sleeplbl.Size = new System.Drawing.Size(35, 13);
+            this.sleeplbl.TabIndex = 21;
+            this.sleeplbl.Text = "label5";
+            // 
+            // outcome
+            // 
+            this.outcome.AutoSize = true;
+            this.outcome.Location = new System.Drawing.Point(359, 220);
+            this.outcome.Name = "outcome";
+            this.outcome.Size = new System.Drawing.Size(92, 13);
+            this.outcome.TabIndex = 22;
+            this.outcome.Text = "Egy átlagos nap:(:";
+            this.outcome.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::pollakszimulator.Properties.Resources.pmszc3e9__1_;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outcome);
+            this.Controls.Add(this.sleeplbl);
+            this.Controls.Add(this.showerlbl);
+            this.Controls.Add(this.hungerlbl);
+            this.Controls.Add(this.happinesslbl);
+            this.Controls.Add(this.intelligencelbl);
+            this.Controls.Add(this.endDaybtn);
+            this.Controls.Add(this.hoursleftlbl);
+            this.Controls.Add(this.storylbl);
+            this.Controls.Add(this.daycount);
+            this.Controls.Add(this.sleeptb);
+            this.Controls.Add(this.sleepbtn);
+            this.Controls.Add(this.showertb);
+            this.Controls.Add(this.showerbtn);
+            this.Controls.Add(this.eattb);
+            this.Controls.Add(this.eatbtn);
+            this.Controls.Add(this.partytb);
+            this.Controls.Add(this.partybtn);
+            this.Controls.Add(this.playetb);
+            this.Controls.Add(this.playbtn);
+            this.Controls.Add(this.studytb);
+            this.Controls.Add(this.studybtn);
+            this.Name = "Form1";
+            this.Text = "PollakSzimulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button studybtn;
+        private System.Windows.Forms.TextBox studytb;
+        private System.Windows.Forms.TextBox playetb;
+        private System.Windows.Forms.Button playbtn;
+        private System.Windows.Forms.TextBox partytb;
+        private System.Windows.Forms.Button partybtn;
+        private System.Windows.Forms.TextBox eattb;
+        private System.Windows.Forms.Button eatbtn;
+        private System.Windows.Forms.TextBox showertb;
+        private System.Windows.Forms.Button showerbtn;
+        private System.Windows.Forms.TextBox sleeptb;
+        private System.Windows.Forms.Button sleepbtn;
+        private System.Windows.Forms.Label daycount;
+        private System.Windows.Forms.Label storylbl;
+        private System.Windows.Forms.Label hoursleftlbl;
+        private System.Windows.Forms.Button endDaybtn;
+        private System.Windows.Forms.Label intelligencelbl;
+        private System.Windows.Forms.Label happinesslbl;
+        private System.Windows.Forms.Label hungerlbl;
+        private System.Windows.Forms.Label showerlbl;
+        private System.Windows.Forms.Label sleeplbl;
+        private System.Windows.Forms.Label outcome;
+    }
+}
+
---
+++ pollakszimulator/pollakszimulator/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pollakszimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intelligence=10;
+        int happiness=10;
+        int sleep=10;
+        int hunger=10;
+        int smell=10;
+        int hours=24;
+        int hoursPlayed;
+        int intelligencetmp;
+        int happinesstmp;
+        int sleeptmp;
+        int hungertmp;
+        int smelltmp;
+        int day;
+        int dtmp = 0;
+        bool in_event = false;
+        int date = 0;
+        int eventnum = 0;
+
+        public void random_encounter()
+        {
+            if (in_event == false )
+            {
+                Random rnd = new Random();
+                int random = rnd.Next(0, 5);
+                storylbl.Text = "Egy átlagos nap :(";
+                outcome.Visible = false;
+                if (random == 0)
+                    first();
+                if (random == 1)
+                {
+                    second();
+                }
+            }
+            else if(eventnum==1)
+            {
+                if (eventnum == 1)
+                    first();
+                if (eventnum == 2)
+                    second();
+                
+            }
+
+        }
+
+        public void first()
+        {
+            in_event = true;
+            eventnum = 1;
+            if (dtmp == 0)
+            {
+                storylbl.Text = "Az iskolába menet megpillantassz egy gyönyörű lányt";
+                happiness += 10;
+                
+            }
+            if (dtmp == 1)
+            {
+                storylbl.Text = "Elhatározod hogy odamész hozzá";
+                outcome.Visible = true;
+                if (smell > 50&&in_event==true)
+                {
+                    outcome.Text = "Ahogy megközelítetted elhányta magát.(lehet meg kéne fürdeni)";
+                    in_event = false;
+                }
+                if (intelligence < 30&&in_event==true)
+                {
+                    outcome.Text = "Sajnos nem sikerült egy értelmes beszélgetést folyatnotok mert retardált vagy";
+                    in_event = false;
+                }
+                else
+                {
+                    outcome.Text = "Sikeresen megbeszélsz vele egy randit ami 6 nap múlva lesz";
+                    date = day + 6;
+                    in_event = false;
+                }
+
+            }
+        }
+        public void second()
+        {
+            in_event = true;
+            eventnum = 2;
+            storylbl.Text = "Az iskolában meghallod hogy Kovi Ati szeretné ha befáradnál az igazgatóiba";
+            Random rnd = new Random();
+            int rnb = rnd.Next(0, 3);
+            if (rnb == 0)
+            {
+                outcome.Visible = true;
+                outcome.Text = "Az iskolát degradáló mémek készítéséért kirugnak bruh";
+                in_event = false;
+            }
+            if(rnb==1||intelligence>50)
+            {
+                outcome.Visible = true;
+                outcome.Text = "Kovi Ati megdicsér a jó iskolai tanulmányaid miatt(nem tudja hogy be se jársz a suliba xd)";
+                in_event = false;
+            }
+            else {
+                outcome.Visible = true;
+                outcome.Text = "Kovi Ati leszidott mert a wc ben maszturbáltál nyitott ajtónál";
+                in_event = false;
+            }
+
+        }
+        public void datefn()
+        {
+            Random rnd = new Random();
+            storylbl.Text = "Randiiii nap<3";
+            int a = rnd.Next(0, 1);
+            if (a == 0)
+            {
+                outcome.Text = "A randin fény derült a lány értelmi fogyatékosságaira(rá ürített az asztalraxd)";
+                happiness -= 50;
+                in_event = false;
+            }
+            if (a== 1){
+                outcome.Text = "A randi nagyszerűen sikerült";
+                happiness += 50;
+                in_event = false;
+            }
+        }
+        private void studybtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                intelligencetmp = (int.Parse(studytb.Text) / 2) + (sleep / 3) - (hunger / 2)-(happiness/6);
+                hours -= int.Parse(studytb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+                studybtn.Enabled = false;
+            }
+            catch (Exception)
+            {
+                studytb.Text = "egy számot írj be";
+                
+            }
+           
+        }
+
+        private void playbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                happinesstmp = int.Parse(playetb.Text)/3 - (hoursPlayed / 10) - (hunger / 2) + (sleep / 3);
+                playbtn.Enabled = false;
+                hours -= int.Parse(playetb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+            }
+            catch (Exception)
+            {
+
+                playetb.Text = "egy számot írj be";
+            }
+        }
+
+        private void endDaybtn_Click(object sender, EventArgs e)
+        {
+            if (hours == 0)
+            {
+                day++;
+                daycount.Text = day + ".nap";
+                hours = 24;
+                hoursPlayed += int.Parse(playetb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+                studybtn.Enabled = true;
+                playbtn.Enabled = true;
+                partybtn.Enabled = true;
+                eatbtn.Enabled = true;
+                showerbtn.Enabled = true;
+                sleepbtn.Enabled = true;
+                studytb.Text = "";
+                playetb.Text = "";
+                partytb.Text = "";
+                eattb.Text = "";
+                showertb.Text = "";
+                sleeptb.Text = "";
+                if (in_event == true)
+                {
+                    dtmp++;
+                }
+                else
+                {
+                    outcome.Visible = false;
+                    outcome.Text = "";
+                    storylbl.Text = "Egy átlagos nap(:";
+                }
+                hunger -= hungertmp;
+                if (hunger < 0)
+                {
+                    hunger = 0;
+                }
+                intelligence += intelligencetmp;
+                happiness += happinesstmp;
+                smell = smelltmp;
+                sleep = sleeptmp;
+                intelligencelbl.Text="inteligencia: "+intelligence;
+                happinesslbl.Text="boldogság: "+happiness;
+                hungerlbl.Text="éhség: "+hunger;
+                showerlbl.Text="tisztaság: "+smell;
+                sleeplbl.Text="álmosság: "+sleep;
+                if (hunger>10)
+                {
+                    storylbl.Text = "Éhenhaltál xd";
+                }
+                if (day == date)
+                {
+                    in_event = true;
+                    datefn();
+                }
+                random_encounter();
+            }
+        }
+
+        private void partybtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                partybtn.Enabled = false;
+                happinesstmp = int.Parse(partytb.Text) / 3 - (hunger / 2) + (sleep / 3);
+                hunger += 2;
+                hours -= int.Parse(partytb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+                
+            }
+            catch (Exception)
+            {
+                partytb.Text = "egy számot adj meg";
+                throw;
+            } 
+        }
+
+        private void eatbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                eatbtn.Enabled = false;
+                hours -= int.Parse(eattb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+                if (hunger > 0)
+                {
+                    hungertmp = int.Parse(eattb.Text) * 5;
+                }
+            }
+            catch
+            {
+                eattb.Text = "egy számot adj meg";
+            }
+        }
+
+        private void showerbtn_Click(object sender, EventArgs e)
+        {
+            
+                showerbtn.Enabled = false;
+                hours -= int.Parse(showertb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+            try
+            {
+                if (int.Parse(showertb.Text) > 0)
+                {
+                    smelltmp = 0;
+                }
+                else
+                {
+                    smelltmp += 10;
+                }
+            }
+            catch
+            {
+                showertb.Text = "egy számot adj meg";
+            }
+        
+        }
+
+        private void sleepbtn_Click(object sender, EventArgs e)
+        {
+            sleepbtn.Enabled = false;
+            try
+            {
+                hours -= int.Parse(sleeptb.Text);
+                hoursleftlbl.Text = hours + "óra van hátra";
+                if (int.Parse(sleeptb.Text) > 8)
+                {
+                    sleeptmp = 100;
+
+                }
+                if (int.Parse(sleeptb.Text) > 4 && int.Parse(sleeptb.Text) < 8)
+                {
+                    sleeptmp = 50;
+
+                }
+                if (int.Parse(sleeptb.Text) > 2 && int.Parse(sleeptb.Text) < 4)
+                {
+                    sleeptmp = 25;
+
+                }
+            }
+            catch
+            {
+                showertb.Text = "egy számot adj meg";
+            }
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            intelligencelbl.Text = "inteligencia: " + intelligence;
+            happinesslbl.Text = "boldogság: " + happiness;
+            hungerlbl.Text = "éhség: " + hunger;
+            showerlbl.Text = "tisztaság: " + smell;
+            sleeplbl.Text = "álmosság: " + sleep;
+            hoursleftlbl.Text = hours + "óra van hátra";
+            daycount.Text = day + ".nap";
+        }
+    }
+}
